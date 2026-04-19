49f87ac2a45e2bf2f2b38a40df13302ab398c3c1
Varshita Tummuri <varshita.tummuri@gmail.com>
2026-04-19 21:19:30 +0530

    changes

--- Backend_AppointmentScheduler/src/main/java/com/example/Backend_AppointmentScheduler/config/CorsConfig.java
+++ Backend_AppointmentScheduler/src/main/java/com/example/Backend_AppointmentScheduler/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins(frontendUrl) //https://therapist-appointment-scheduler-ayyxeyqxk.vercel.app/
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
